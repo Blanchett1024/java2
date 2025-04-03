76fabb532dede6a4d311da40ae5ef1de65e330f9
Blanchett1024 <dy10246@naver.com>
2025-04-03 10:51:44 +0900

    1교시 마무리

---
+++ src/DoWhileSample.java
@@ -0,0 +1,11 @@
+public class DoWhileSample {
+    public static void main(String[] args) {
+        char a='a';
+        
+        do{
+            System.out.println(a);
+            a=(char)(a+1);
+        }
+        while(a<='z');
+    }
+}

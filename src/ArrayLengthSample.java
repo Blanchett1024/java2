b68c265d3763c947b0a5d47a620c18d309705b16
Blanchett1024 <dy10246@naver.com>
2025-04-03 12:35:30 +0900

    3교시 마무리

---
+++ src/ArrayLengthSample.java
@@ -0,0 +1,11 @@
+public class ArrayLengthSample {
+    public static void main(String[] args) {
+        
+        int intArray[] = new int[5];
+        int size = intArray.length;
+
+        for(int i=0; i<intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
+}
